d00baed7adbf0f7a20be136bed66c5adc0d1ab9a
nicomellon <nmellon94@gmail.com>
2021-11-11 22:31:46 +0100

    labs done

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI =
+  "mongodb+srv://nico:NydPTjv8voDT7lEN@cluster0.d3xqw.mongodb.net/RecipesLab?retryWrites=true&w=majority";
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+const createRecipe = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the database");
+    // new recipe
+    /* const newRecipe = {
+      title: "Tortilla de patata",
+      level: "Amateur Chef",
+      ingredients: ["Potatoes", "Onions", "Eggs"],
+      cuisine: "Mediterranean",
+      dishType: "main_course",
+      image:
+        "https://lh3.googleusercontent.com/proxy/tMOcxIlsnaO3WmXJtoKTIj6kDF0JIkZ-BihuejQkrN72I4ltI2Map9lISu4KcIz3lr7Fxgcyvs8rr2yHt6haPK5jJOOAUU9KKeMoRnsj5Gk20Z2OXvNC0FyUapFEwljdxfD90ILLhH51-1AMp-eYaffIBg",
+      duration: 40,
+      creator: "España",
+      created: "03/25/1798",
+    };
+    console.log(newRecipe);
+    await Recipe.create(newRecipe); */
+    // multiplte recipes from data
+    // await Recipe.insertMany(data);
+    /* await Recipe.findOneAndUpdate(
+      {
+        title: "Rigatoni alla Genovese",
+      },
+      {
+        duration: 100,
+      }
+    ); */
+    await Recipe.deleteOne({
+      title: "Carrot Cake",
+    });
+    //disconnect
+    mongoose.disconnect();
+    console.log("disconnected");
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+  }
+};
+
+createRecipe();
